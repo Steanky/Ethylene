95e95b174c17ac777a5e5d6c5aa4b9fee673f487
John-DND <dnd.initiative.tracker@gmail.com>
2022-01-17 15:18:47 -0800

    optimize imports

--- ethylene-core/src/main/java/com/github/steanky/ethylene/core/bridge/ConfigBridges.java
+++ ethylene-core/src/main/java/com/github/steanky/ethylene/core/bridge/ConfigBridges.java
@@ -1,10 +1,10 @@
 package com.github.steanky.ethylene.core.bridge;
 
-import com.github.steanky.ethylene.core.util.CallableUtils;
 import com.github.steanky.ethylene.core.ConfigElement;
 import com.github.steanky.ethylene.core.codec.ConfigCodec;
 import com.github.steanky.ethylene.core.collection.ConfigNode;
 import com.github.steanky.ethylene.core.collection.FileConfigNode;
+import com.github.steanky.ethylene.core.util.CallableUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
