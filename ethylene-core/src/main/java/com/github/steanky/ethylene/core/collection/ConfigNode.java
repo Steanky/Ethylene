e27b048ae6e4fcd7fb53bf8d9d17d46d50b14e2f
steank <dnd.initiative.tracker@gmail.com>
2022-06-27 05:33:04 -0700

    more convenience overloads + version bump

--- ethylene-core/src/main/java/com/github/steanky/ethylene/core/collection/ConfigNode.java
+++ ethylene-core/src/main/java/com/github/steanky/ethylene/core/collection/ConfigNode.java
@@ -23,4 +23,34 @@ public interface ConfigNode extends ConfigElement, Map<String, ConfigElement>, C
     default @NotNull ConfigNode asNode() {
         return this;
     }
+
+    /**
+     * Convenience overload for {@link ConfigNode#put(Object, Object)}. The provided string value will be wrapped in a
+     * new {@link ConfigPrimitive} and added to this node.
+     * @param key the key to be associated with this value
+     * @param value the value to add to the node
+     */
+    default void putString(@NotNull String key, String value) {
+        put(key, new ConfigPrimitive(value));
+    }
+
+    /**
+     * Convenience overload for {@link ConfigNode#put(Object, Object)}. The provided number value will be wrapped in a
+     * new {@link ConfigPrimitive} and added to this node.
+     * @param key the key to be associated with this value
+     * @param value the value to add to the node
+     */
+    default void putNumber(@NotNull String key, Number value) {
+        put(key, new ConfigPrimitive(value));
+    }
+
+    /**
+     * Convenience overload for {@link ConfigNode#put(Object, Object)}. The provided char value will be wrapped in a
+     * new {@link ConfigPrimitive} and added to this node.
+     * @param key the key to be associated with this value
+     * @param value the value to add to the node
+     */
+    default void putCharacter(@NotNull String key, char value) {
+        put(key, new ConfigPrimitive(value));
+    }
 }
