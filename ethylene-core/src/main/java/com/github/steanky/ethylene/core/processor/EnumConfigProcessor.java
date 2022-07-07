feed84f00c271ec656638e9fc262dbecaac2d64c
steank <dnd.initiative.tracker@gmail.com>
2022-07-07 10:46:27 -0700

    added mapProcessor, setProcessor convenience methods, EnumConfigProcessor has optional case sensitivity

--- ethylene-core/src/main/java/com/github/steanky/ethylene/core/processor/EnumConfigProcessor.java
+++ ethylene-core/src/main/java/com/github/steanky/ethylene/core/processor/EnumConfigProcessor.java
@@ -5,6 +5,7 @@ import com.github.steanky.ethylene.core.ConfigPrimitive;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
@@ -15,15 +16,28 @@ import java.util.function.Function;
  */
 class EnumConfigProcessor<TEnum extends Enum<?>> implements ConfigProcessor<TEnum> {
     private final Class<? extends TEnum> enumClass;
+    private final boolean caseSensitive;
 
     private Function<String, TEnum> lookupFunction;
 
     /**
-     * Creates a new EnumConfigProcessor, which will be able to process instances of the provided enum class.
+     * Creates a new EnumConfigProcessor, which will be able to process instances of the provided enum class. The
+     * processor will be case-sensitive.
      * @param enumClass the enum class used to provide a list of enum constants
      */
     EnumConfigProcessor(@NotNull Class<? extends TEnum> enumClass) {
+        this(enumClass, true);
+    }
+
+    /**
+     * Creates a new EnumConfigProcessor, which will be able to process instances of the provided enum class, and with
+     * the provided case sensitivity handling.
+     * @param enumClass the enum class used to provide a list of enum constants
+     * @param caseSensitive whether this processor should be case-sensitive
+     */
+    EnumConfigProcessor(@NotNull Class<? extends TEnum> enumClass, boolean caseSensitive) {
         this.enumClass = Objects.requireNonNull(enumClass);
+        this.caseSensitive = caseSensitive;
     }
 
     @Override
@@ -59,15 +73,16 @@ class EnumConfigProcessor<TEnum extends Enum<?>> implements ConfigProcessor<TEnu
             if(constants.length > 10) {
                 Map<String, TEnum> lookupMap = new HashMap<>(constants.length);
                 for(TEnum constant : constants) {
-                    lookupMap.put(constant.toString(), constant);
+                    lookupMap.put(caseSensitive ? constant.name() : constant.name().toLowerCase(Locale.ROOT), constant);
                 }
 
-                lookupFunction = lookupMap::get;
+                lookupFunction = caseSensitive ? lookupMap::get : s -> lookupMap.get(s.toLowerCase(Locale.ROOT));
             }
             else {
                 lookupFunction = constantName -> {
                     for(TEnum constant : constants) {
-                        if(constant.toString().equals(constantName)) {
+                        if(caseSensitive ? constant.name().equals(constantName) : constant.name()
+                                .toLowerCase(Locale.ROOT).equals(constantName.toLowerCase(Locale.ROOT))) {
                             return constant;
                         }
                     }
